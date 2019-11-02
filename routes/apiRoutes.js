76715bb5af06fbb5f4fcda2af74df89bb727c0f8
tomrennhack <tomrennhack@gmail.com>
2019-11-02 10:34:06 -0400

    sign up page formatted and signup login btns added

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,20 +1,12 @@
 var db = require("../models");
 
 module.exports = function(app) {
-
   require("./post-signup.js")(app);
 
   // Get all examples
   app.get("/api/examples", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
       res.json(dbExamples);
-    });
-  });
-
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
     });
   });
 
