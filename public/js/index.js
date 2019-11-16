4f2ce0c2998dfa0cc9a493e2660fc297ef26ec66
tomrennhack <tomrennhack@gmail.com>
2019-11-16 15:23:22 -0500

    just sendgrid user ID outstanding, maybe

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,16 @@
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey('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');
+
 // Get references to page elements
 var $exampleText = $("#example-text");
 var $exampleDescription = $("#example-description");
 var $submitBtn = $("#submit");
 var $exampleList = $("#example-list");
+var $sendFriendEmail = $("#mark-incomplete");
 
 // The API object contains methods for each kind of request we'll make
 var API = {
-  saveExample: function(example) {
+  saveExample: function (example) {
     return $.ajax({
       headers: {
         "Content-Type": "application/json"
@@ -16,13 +20,13 @@ var API = {
       data: JSON.stringify(example)
     });
   },
-  getExamples: function() {
+  getExamples: function () {
     return $.ajax({
       url: "api/examples",
       type: "GET"
     });
   },
-  deleteExample: function(id) {
+  deleteExample: function (id) {
     return $.ajax({
       url: "api/examples/" + id,
       type: "DELETE"
@@ -31,9 +35,9 @@ var API = {
 };
 
 // refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function() {
-  API.getExamples().then(function(data) {
-    var $examples = data.map(function(example) {
+var refreshExamples = function () {
+  API.getExamples().then(function (data) {
+    var $examples = data.map(function (example) {
       var $a = $("<a>")
         .text(example.text)
         .attr("href", "/example/" + example.id);
@@ -61,7 +65,7 @@ var refreshExamples = function() {
 
 // handleFormSubmit is called whenever we submit a new example
 // Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
+var handleFormSubmit = function (event) {
   event.preventDefault();
 
   var example = {
@@ -74,7 +78,7 @@ var handleFormSubmit = function(event) {
     return;
   }
 
-  API.saveExample(example).then(function() {
+  API.saveExample(example).then(function () {
     refreshExamples();
   });
 
@@ -84,16 +88,36 @@ var handleFormSubmit = function(event) {
 
 // handleDeleteBtnClick is called when an example's delete button is clicked
 // Remove the example from the db and refresh the list
-var handleDeleteBtnClick = function() {
+var handleDeleteBtnClick = function () {
   var idToDelete = $(this)
     .parent()
     .attr("data-id");
 
-  API.deleteExample(idToDelete).then(function() {
+  API.deleteExample(idToDelete).then(function () {
     refreshExamples();
   });
 };
 
+var sendFriendEmails = function () {
+  // var userEmail = require("../models/userInfo")
+  // function grabEmail() {
+  console.log("found email");
+  const msg = {
+    to: 'tomrennhack@gmail.com',
+    from: 'yourfkntodolist@gmail.com',
+    subject: "I'm a failure",
+    text: 'this is my test email',
+    html: '<strong>I failed to accomplish my tasks today - what a loser! </strong>',
+  };
+  sgMail.send(msg);
+  console.log("sent email");
+}
+// module.exports = grabEmail();
+
 // Add event listeners to the submit and delete buttons
 $submitBtn.on("click", handleFormSubmit);
 $exampleList.on("click", ".delete", handleDeleteBtnClick);
+$sendFriendEmail.on("click", sendFriendEmails);
+
+// export email function
+// module.exports = sendFriendEmails();
